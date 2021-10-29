e7b039f8ed506b3b8755292c41d52dd96d60d034
TomRoberts97 <37558792+TomRoberts97@users.noreply.github.com>
2021-10-29 10:55:27 +0100

    Set up folder structure

---
+++ Toms API App/Boilerplate/ApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toms_API_App.Boilerplate
+{
+    public class ApiConstants
+    {
+        public const string BaseApiUrl = "https://api.nasa.gov";
+        public const string ApiKey = "96CZNUQO24dd4RHsHyQndZ2e7DLpAQSpOMGr2K4U";
+        public const string ApodServiceEndpoint = "planetary/apod";
+    }
+}
---
+++ Toms API App/Boilerplate/Locator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toms_API_App.Boilerplate
+{
+    class Locator
+    {
+    }
+}
