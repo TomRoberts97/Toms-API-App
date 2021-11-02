5e3a7a247ec21d1e46d11fb1452cfd6aa20766bc
TomRoberts97 <37558792+TomRoberts97@users.noreply.github.com>
2021-11-02 16:34:36 +0000

    Added beginnings of rest service

---
+++ Toms API App/Services/Rest/IRestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toms_API_App.Services.Rest
+{
+    public interface IRestService
+    {
+        Task<(ResultStatus status, TResponse payload, string rawResponse)> GetAsync<TResponse>(string url);
+        Task<(ResultStatus status, TResponse payload, string rawResponse)> PostAsync<TRequest, TResponse>(TRequest request, string url);
+    }
+
+    public enum ResultStatus
+    {
+        Success = 0,
+        ConnectionFailed,
+        Unauthorized,
+        BadResponse,
+        BadPayload,
+        // Map HttpStatus codes to additional enum members here ...
+        Other
+    }
+}
---
+++ Toms API App/Services/Rest/RestService.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toms_API_App.Services.Rest
+{
+    class RestService : IRestService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _host;
+
+        public RestService(HttpClient httpClient, string baseUrl)
+        {
+            _httpClient = httpClient;
+            _host = baseUrl;
+        }
+
+        public async Task<(ResultStatus status, TResponse payload, string rawResponse)> GetAsync<TResponse>(string path)
+        {
+            string uri = Path.Combine(_host, path);
+
+            HttpResponseMessage response = await _httpClient.GetAsync(Sanitise(uri));
+
+            if (response.IsSuccessStatusCode == true)
+            {
+                string rawData = await response.Content.ReadAsStringAsync();
+
+                // Turn our JSON string into a csharp object (or object-graph)
+                var result = JsonConvert.DeserializeObject<TResponse>(rawData);
+
+                // Return a response of type TResult.
+                return (ResultStatus.Success, result, rawData);
+            }
+            else
+            {
+                return (ResultStatus.Other, default(TResponse), null);
+            }
+        }
+
+        public async Task<(ResultStatus status, TResponse payload, string rawResponse)> PostAsync<TRequest, TResponse>(TRequest request, string path)
+        {
+            string uri = Path.Combine(_host, path);
+
+            string json = JsonConvert.SerializeObject(request);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(Sanitise(uri), data);
+
+            if (response.IsSuccessStatusCode == true)
+            {
+                string rawData = await response.Content.ReadAsStringAsync();
+
+                // Turn our JSON string into a csharp object (or object-graph)
+                TResponse result = JsonConvert.DeserializeObject<TResponse>(rawData);
+
+                // Return a response of type TResult.
+                return (ResultStatus.Success, result, rawData);
+
+            }
+            else
+            {
+                return (ResultStatus.Other, default(TResponse), null);
+            }
+        }
+
+        private string Sanitise(string uri)
+        {
+            return uri.Replace('\\', '/');
+        }
+    }
+}
